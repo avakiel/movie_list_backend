e543a22bf11755ad36b382fe4dd87d4cc067a60b
avakiel <79792776+avakiel@users.noreply.github.com>
2024-04-16 15:06:19 +0300

    cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 const { moviesRouter } = require('./Routes/moviesRouter');
 const { favouriteRouter } = require('./Routes/favouriteRouter');
 const { futureRouter } = require('./Routes/futureRouter');
@@ -9,6 +10,7 @@ const { watchedRouter } = require('./Routes/watchedRouter');
 function createServer() {
   const server = express();
 
+server.use(cors());
 server.use('/movies', moviesRouter)
 server.use('/favourite', favouriteRouter)
 server.use('/future', futureRouter)
